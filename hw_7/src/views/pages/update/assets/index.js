967e091289cd0c030f505649e9af1f5992a291c7
Анастасия Замашнюк <a.zamashnyuk@onpoint.ru>
2022-01-05 15:51:49 +0300

    small fix

--- hw_7/src/views/pages/update/assets/index.js
+++ hw_7/src/views/pages/update/assets/index.js
@@ -1,12 +1,10 @@
 const updateForm = document.querySelector('.form-update-book');
 
 updateForm.addEventListener('submit', (e) => {
-  console.log('aaa');
   e.preventDefault();
   const formData = new FormData(updateForm);
 
   const id = window.location.href.split('/update/')[1];
-  console.log(`http://localhost:5000/api/book/${id}`);
 
   fetch(`http://localhost:5000/api/book/${id}`, {
     method: 'PUT',
